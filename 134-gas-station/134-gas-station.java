a5f4e46de9317f1df824cbee0bfc3c5021660db8
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-01-21 17:16:33 +0530

    Time: 2 ms (65.41%), Space: 64.1 MB (18.51%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,24 +1,44 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        //Two Pass Solution
         
-        //determine if we have a solution
-        int total = 0;
-        for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
-        }
-        if (total < 0) {
-            return -1;
-        }
+//         //determine if we have a solution
+//         int total = 0;
+//         for (int i = 0; i < gas.length; i++) {
+//             total += gas[i] - cost[i];
+//         }
+//         if (total < 0) {
+//             return -1;
+//         }
         
-        // find out where to start
-        int gasInCar = 0;
-        int startingIndex = 0;
-        for (int i = 0; i < gas.length;i++) {
-            gasInCar += gas[i] - cost[i];
-            if (gasInCar < 0) {
-                startingIndex = i + 1;
-                gasInCar = 0;
+//         // find out where to start
+//         int gasInCar = 0;
+//         int startingIndex = 0;
+//         for (int i = 0; i < gas.length;i++) {
+//             gasInCar += gas[i] - cost[i];
+//             if (gasInCar < 0) {
+//                 startingIndex = i + 1;
+//                 gasInCar = 0;
+//             }
+//         }
+//         return startingIndex;
+        
+        //One Pass Solution
+        int startingIndex=0;
+        int totalGas=0;
+        int totalCost=0;
+        int gasInCar=0;
+        for(int i=0;i<gas.length;i++){
+            totalGas+=gas[i];
+            totalCost+=cost[i];
+            gasInCar+=(gas[i]-cost[i]);
+            if(gasInCar<0){
+                startingIndex=i+1;
+                gasInCar=0;
             }
+        }
+        if(totalGas<totalCost){
+            return -1;
         }
         return startingIndex;
     }
