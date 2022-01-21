dcfdfc317c4534a995fa0ac7d75b48518903540b
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-01-21 17:07:35 +0530

    Time: 2 ms (65.41%), Space: 63.9 MB (34.97%) - LeetHub

---
+++ 134-gas-station/134-gas-station.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+        //determine if we have a solution
+        int total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+        }
+        if (total < 0) {
+            return -1;
+        }
+        
+        // find out where to start
+        int gasInCar = 0;
+        int startingIndex = 0;
+        for (int i = 0; i < gas.length;i++) {
+            gasInCar += gas[i] - cost[i];
+            if (gasInCar < 0) {
+                startingIndex = i + 1;
+                gasInCar = 0;
+            }
+        }
+        return startingIndex;
+    }
+}
