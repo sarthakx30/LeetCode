14539035018e6784f38e3bba9df0ce432144fe0a
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-01-22 17:15:52 +0530

    Time: 0 ms (100.00%), Space: 38.2 MB (85.58%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -7,18 +7,24 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
+            //Checks If Left Part Is Sorted
             if(nums[low]<=nums[mid]){
+                //If target even exists in the left Part
                 if(target>=nums[low] && target<nums[mid]){
                     high=mid-1;
                 }
+                //Exists In Right Part
                 else{
                     low=mid+1;
                 }
             }
+            //If Right Part is Sorted
             else{
+                //Checks is Target Exists In Right Part
                 if(target>nums[mid] && target<=nums[high]){
                     low=mid+1;
                 }
+                //Exists In Left Part
                 else{
                     high=mid-1;
                 }
