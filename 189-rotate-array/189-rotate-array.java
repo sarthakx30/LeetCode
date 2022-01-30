4248a0fd6174e650e9dab7c2cbb460166f681546
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-01-31 02:01:09 +0530

    Time: 1 ms (76.91%), Space: 64.6 MB (6.09%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        //O(n*k) time complexity
+        //O(1) space complexity
+        // while(k>0){
+        //     int temp=nums[nums.length-1];
+        //     for(int i=nums.length-1;i>0;i--){
+        //         nums[i]=nums[i-1];
+        //     }
+        //     nums[0]=temp;
+        //     k--;
+        // }
+        
+        //O(n) time complexity
+        //O(1) space complexity
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
