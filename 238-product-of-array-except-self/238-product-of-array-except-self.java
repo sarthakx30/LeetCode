1dbb32eba3f89e4306fc0d91b6a61ed30806810a
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-02-01 19:37:36 +0530

    Time: 3 ms (44.43%), Space: 57 MB (33.15%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]=new int[nums.length];
+        Map<Integer,Integer> map=new HashMap<>();
+        int prodOfArray=1;
+        int zeroCount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                zeroCount++;
+            }
+            else{
+                prodOfArray*=nums[i];
+            }
+        }
+        if(zeroCount>1){
+            return ans;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(zeroCount==1 && nums[i]==0){
+                ans[i]=prodOfArray;
+            }
+            else if(zeroCount==1 && nums[i]!=0){
+                ans[i]=0;
+            }
+            else{
+                ans[i]=prodOfArray/nums[i];
+            }
+        }
+        return ans;
+    }
+}
