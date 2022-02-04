08728b6792d673cd298664e31f652f02bcd44318
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-02-04 21:33:09 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (8.86%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -10,16 +10,39 @@ class Node {
 
 class Solution {
     public Node flatten(Node head) {
-        Stack<Node> s=new Stack<>();
+        // My original solution with O(n) Space Complexity
+        // Stack<Node> s=new Stack<>();
+        // Node curr=head;
+        // while(curr!=null){
+        //     if(curr.next==null && s.isEmpty()==false){
+        //         curr.next=s.pop();
+        //         curr.next.prev=curr;
+        //     }
+        //     if(curr.child!=null){
+        //         if(curr.next!=null){
+        //             s.push(curr.next);
+        //         }
+        //         curr.next=curr.child;
+        //         curr.next.prev=curr;
+        //         curr.child=null;
+        //     }
+        //     curr=curr.next;
+        // }
+        // return head;
+        // O(1) Space Complexity
+        if(head==null){
+            return head;
+        }
         Node curr=head;
         while(curr!=null){
-            if(curr.next==null && s.isEmpty()==false){
-                curr.next=s.pop();
-                curr.next.prev=curr;
-            }
             if(curr.child!=null){
+                Node temp=curr.child;
+                while(temp.next!=null){
+                    temp=temp.next;
+                }
+                temp.next=curr.next;
                 if(curr.next!=null){
-                    s.push(curr.next);
+                    curr.next.prev=temp;
                 }
                 curr.next=curr.child;
                 curr.next.prev=curr;
