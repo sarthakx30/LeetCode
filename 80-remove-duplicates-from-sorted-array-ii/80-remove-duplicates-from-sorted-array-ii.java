a3fe8e41ee0f65ec7bdc1aeaabf583fede6b76e3
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-02-06 16:37:45 +0530

    Time: 1 ms (65.03%), Space: 44.8 MB (6.26%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,23 +1,31 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length <= 2) return nums.length;
-        int ind=1;
-        int i=1;
-        int count=1;
-        while(i<nums.length){
-            if(nums[i]==nums[i-1]){
-                if(count<2){
-                    count++;
-                    nums[ind]=nums[i];
-                    ind++;
-                }
+        // if (nums.length <= 2) return nums.length;
+        // int ind=1;
+        // int i=1;
+        // int count=1;
+        // while(i<nums.length){
+        //     if(nums[i]==nums[i-1]){
+        //         if(count<2){
+        //             count++;
+        //             nums[ind]=nums[i];
+        //             ind++;
+        //         }
+        //     }
+        //     else{
+        //         count=1;
+        //         nums[ind]=nums[i];
+        //         ind++;
+        //     }
+        //     i++;
+        // }
+        // return ind;
+        int ind = 0;
+        for (int i=0;i<nums.length;i++){
+            //if its the first number being repeated or the current number is a different one we might have seen 2                 places ago beacuse it is a sorted array
+            if (ind < 2 || nums[i] > nums[ind-2]){
+                nums[ind++] = nums[i];
             }
-            else{
-                count=1;
-                nums[ind]=nums[i];
-                ind++;
-            }
-            i++;
         }
         return ind;
     }
