213cea147fdf421ae183716a6312a9f081a192b4
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-07-23 15:27:32 +0530

    Time: 2 ms (78.80%), Space: 52.1 MB (70.12%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,19 +1,41 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+//         int leftSum=0;
+//         int rightSum=0;
+//         for(int i=0;i<nums.length;i++){
+//             rightSum+=nums[i];
+//         }
+//         int index=-1;
+//         for(int i=0;i<nums.length;i++){
+//             leftSum+=nums[i];
+//             //System.out.println(leftSum+" "+rightSum);
+//             if(leftSum==rightSum){
+//                 index=i;
+//                 break;
+//             }
+//             rightSum-=nums[i];
+//         }
+//         return index;
+//     }
+// }
+
+class Solution{
+    public int pivotIndex(int[] nums){
+        int index=-1;
         int leftSum=0;
         int rightSum=0;
         for(int i=0;i<nums.length;i++){
             rightSum+=nums[i];
         }
-        int index=-1;
         for(int i=0;i<nums.length;i++){
             leftSum+=nums[i];
-            //System.out.println(leftSum+" "+rightSum);
             if(leftSum==rightSum){
                 index=i;
                 break;
             }
-            rightSum-=nums[i];
+            else{
+                rightSum-=nums[i];
+            }
         }
         return index;
     }
