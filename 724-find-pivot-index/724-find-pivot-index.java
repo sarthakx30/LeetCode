792d8b22916cee7f7dd0ed18a062098169782e1c
Sarthak Kharabanda <66181354+sarthakx30@users.noreply.github.com>
2022-07-23 15:28:03 +0530

    Time: 2 ms (78.80%), Space: 51.9 MB (77.54%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,24 +1,3 @@
-// class Solution {
-//     public int pivotIndex(int[] nums) {
-//         int leftSum=0;
-//         int rightSum=0;
-//         for(int i=0;i<nums.length;i++){
-//             rightSum+=nums[i];
-//         }
-//         int index=-1;
-//         for(int i=0;i<nums.length;i++){
-//             leftSum+=nums[i];
-//             //System.out.println(leftSum+" "+rightSum);
-//             if(leftSum==rightSum){
-//                 index=i;
-//                 break;
-//             }
-//             rightSum-=nums[i];
-//         }
-//         return index;
-//     }
-// }
-
 class Solution{
     public int pivotIndex(int[] nums){
         int index=-1;
